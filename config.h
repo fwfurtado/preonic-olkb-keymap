c263628c63d26764ee2fd9b15251c6af091a6bb8
Fernando <fwfurtado@gmail.com>
2024-01-24 13:58:47 -0300

    fix: build

--- config.h
+++ config.h
@@ -16,15 +16,17 @@
 
 #pragma once
 
-#include "custom_config.h"
+// #include "custom_config.h"
 
+// Enable more layers
+#undef DYNAMIC_KEYMAP_LAYER_COUNT
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
 
 // default but used in macros
 #undef TAPPING_TERM
 #define TAPPING_TERM 200
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
+
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
